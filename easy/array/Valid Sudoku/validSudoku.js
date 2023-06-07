1e64fce1a4cd54a719b6610f2c90cee6a7b0e182
Oleksandr <lysakova@yahoo.com>
2023-06-07 05:12:59 +0200

    reverseString

--- easy/array/Valid Sudoku/validSudoku.js
+++ easy/array/Valid Sudoku/validSudoku.js
@@ -10,4 +10,29 @@ const board = [
   ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
 ];
 
-var isValidSudoku = function (board) {};
+// const firstColumn = [0][0], [0][3], [0][6] - [3][0], [3][3], [3][6] - [6][0], [6][3], [6][6]
+// const secondColumn = [0][1], [0][4], [0][7] - [3][1], [3][4], [3][7] - [6][1], [6][4], [6][7]
+// const thirdColumn = [0][2], [0][5], [0][8] - [3][2], [3][5], [3][6] - [6][2], [6][5], [6][8]
+
+const isNoDuplicates = (arr) => {
+  const arrayOfNumbers = arr.filter((item) => {
+    return !Number.isNaN(+item);
+  });
+
+  return arrayOfNumbers.length === new Set(arrayOfNumbers).size;
+};
+
+var isValidSudoku = function (board) {
+  const rows = Array(9).fill([]);
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      rows[j].push(board[i][j]);
+    }
+    // if (!isNoDuplicates(board[i])) return false;
+  }
+  console.log(rows);
+  return true;
+};
+
+module.exports = { isValidSudoku, isNoDuplicates };
--- easy/array/Valid Sudoku/validSudoku.test.js
+++ easy/array/Valid Sudoku/validSudoku.test.js
@@ -1,4 +1,4 @@
-import isValidSudoku from './validSudoku';
+const { isValidSudoku, isNoDuplicates } = require('./validSudoku');
 
 const board = [
   ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
@@ -12,8 +12,49 @@ const board = [
   ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
 ];
 
+const boardFalse = [
+  ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+  ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+  ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+  ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+  ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+  ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+  ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+  ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+  ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+];
+
 describe('valid sudoku', () => {
   test('return boolean', () => {
-    expect(isValidSudoku(board));
+    expect(typeof isValidSudoku(board)).toBe('boolean');
+  });
+  test('return true', () => {
+    expect(isValidSudoku(board)).toBeTruthy();
+  });
+  test('return false', () => {
+    expect(isValidSudoku(boardFalse)).toBeFalsy();
+  });
+});
+
+describe('isNoDuplicate finds duplicates in array', () => {
+  const arrWithDuplicates = [1, 2, 3, 3];
+  const arrWithNoDuplicates = [1, 2, 5];
+  const arrWithNoDuplicatesAndWithDots = [1, 2, '.', 5, '.'];
+  const arrWithDuplicatesAndWithDots = [1, 2, 3, 3, '.', 5, '.'];
+
+  test('return bolean', () => {
+    expect(typeof isNoDuplicates(arrWithDuplicates)).toBe('boolean');
+  });
+  test('return true', () => {
+    expect(isNoDuplicates(arrWithNoDuplicates)).toBeTruthy();
+  });
+  test('return false', () => {
+    expect(isNoDuplicates(arrWithDuplicates)).toBeFalsy();
+  });
+  test('return true, no duplicates, are dots', () => {
+    expect(isNoDuplicates(arrWithNoDuplicatesAndWithDots)).toBeTruthy();
+  });
+  test('return false, are duplicates with dots', () => {
+    expect(isNoDuplicates(arrWithDuplicatesAndWithDots)).toBeFalsy();
   });
 });
