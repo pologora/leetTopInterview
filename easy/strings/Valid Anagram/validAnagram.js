fc31e63476d7efb60204c08d315acc099bbf158b
Oleksandr <lysakova@yahoo.com>
2023-06-12 07:04:14 +0200

    Valid anagram solution

--- easy/strings/Valid Anagram/validAnagram.js
+++ easy/strings/Valid Anagram/validAnagram.js
@@ -1,18 +1,15 @@
 module.exports = (s, t) => {
   if (!s || !t) return false;
-  if (s.length !== t.length) return false;
-
-  const hashTableS = {};
-  const hashTableT = {};
+  const hashTable = {};
 
   for (let i = 0; i < s.length; i++) {
-    hashTableS[s[i]] = (hashTableS[s[i]] ? hashTableS[s[i]] : 0) + 1;
-    hashTableT[t[i]] = (hashTableT[t[i]] ? hashTableT[t[i]] : 0) + 1;
+    hashTable[s[i]] = (hashTable[s[i]] || 0) + 1;
+    hashTable[t[i]] = (hashTable[t[i]] || 0) - 1;
   }
 
   // eslint-disable-next-line no-restricted-syntax
-  for (const key in hashTableS) {
-    if (hashTableS[key] !== hashTableT[key]) return false;
+  for (const key in hashTable) {
+    if (hashTable[key] !== 0) return false;
   }
 
   return true;
