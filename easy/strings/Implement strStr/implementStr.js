aef124c2f7a03749087a27bb980f520b6cafdaad
Oleksandr <lysakova@yahoo.com>
2023-06-17 06:38:52 +0200

    Haystack needle O(N^2) solution

--- easy/strings/Implement strStr/implementStr.js
+++ easy/strings/Implement strStr/implementStr.js
@@ -1,4 +1,23 @@
 module.exports = (haystack, needle) => {
-  console.log(haystack);
-  return needle;
+  if (needle.length > haystack.length) return -1;
+
+  let needleCharIndex = 0;
+  let firstIndex = null;
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      firstIndex = i;
+
+      while (haystack[i] === needle[needleCharIndex]) {
+        i += 1;
+        needleCharIndex += 1;
+        if (needleCharIndex === needle.length) return firstIndex;
+      }
+
+      i = firstIndex;
+      needleCharIndex = 0;
+    }
+  }
+
+  return -1;
 };
--- easy/strings/Implement strStr/implementStr.test.js
+++ easy/strings/Implement strStr/implementStr.test.js
@@ -6,4 +6,28 @@ describe('neddle and haystack strings', () => {
     const needle = 'sad';
     expect(typeof implementStr(haystack, needle)).toBe('number');
   });
+
+  test('should return index of the first occurrence if needle is a part of haystack', () => {
+    const haystack = 'sadbutsad';
+    const needle = 'sad';
+    expect(implementStr(haystack, needle)).toBe(0);
+  });
+
+  test('should return index of the first occurrence if needle is a part of haystack', () => {
+    const haystack = 'gogoram';
+    const needle = 'ram';
+    expect(implementStr(haystack, needle)).toBe(4);
+  });
+
+  test('should return -1 if needle is not a part of a haystack', () => {
+    const haystack = 'leetcode';
+    const needle = 'leeto';
+    expect(implementStr(haystack, needle)).toBe(-1);
+  });
+
+  test('should return index of the first occurrence if needle is a part of haystack', () => {
+    const haystack = 'mississippi';
+    const needle = 'issip';
+    expect(implementStr(haystack, needle)).toBe(4);
+  });
 });
